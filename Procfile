93998815bfc760d3286d8713967a959902fa69c2
Emma Chambers <emma.chambers@kcl.ac.uk>
2019-05-23 18:54:04 +0100

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn coding_london:app
--- hello.py
+++ hello.py
@@ -1,20 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
-app = Flask("MyApp")
+app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello World"
+   return render_template("helloworld.html")
 
 @app.route("/<name>")
-def hello_someone(name):
-            return render_template("hello.html", name=name.title())
-            
-@app.route("/signup", methods=["POST"])
-def sign_up():
-    form_data = request.form
-    email = form_data["email"]
-    print(email)
-    return "All OK, "+email
-
-app.run(debug=False)
+def hi(name):
+   return render_template("helloworld.html", userName=name)
